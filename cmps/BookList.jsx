ade0327d2bf31ae5f59ede79f4205609ace9c46a
kashikarin <karinkashi@gmail.com>
2025-06-04 14:22:09 +0300

    apply small design fixes

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -6,6 +6,9 @@ export function BookList ({books}){
             {books && books.map((book, i) => (
                 <li key={book.id}>
                     <BookPreview book={book} idx={i}/>
+                    <section className="book-preview-options">
+                        <button className="book-details-button">Details</button>
+                    </section>
                 </li>
             ))}
         </ul>
